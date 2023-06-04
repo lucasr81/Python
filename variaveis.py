c77db7844415767225f19ac2a71142f43dde01e6
Lucas <83793265+lucasr81@users.noreply.github.com>
2023-06-04 00:22:57 -0300

    Create variaveis.py
    Estudos sobre variaveis

---
+++ variaveis.py
@@ -0,0 +1,21 @@
+""" 
+Variavies são um recurso usado pelas linguagem de programação
+para armazenar valores.
+Isso permiti o python manipular variaveis basicas como strings (palavras ou cadeias de caracteres), 
+inteiros e reais (float)
+"""
+#Exemplos:
+
+mensagem = 'Exemplo de mensagem' 
+print(mensagem)
+
+n = 25
+print(n)
+
+pi = 3.141592653589931
+print(pi)
+
+"""
+Aqui foram feitas três atribuições. A primeira foi  uma string para uma variável chamada mensagem. 
+a variavel 2 foi atribuído o valor inteiro 25 para n, e na terceira variavel, um valor decimal  pi.
+"""
